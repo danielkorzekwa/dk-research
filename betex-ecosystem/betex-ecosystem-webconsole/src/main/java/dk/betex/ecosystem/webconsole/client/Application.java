95b7be32a65b84778eaca7ce7bc5d37dd35efa5f
daniel.korzekwa <daniel.korzekwa@2d2c5f88-d9fa-11de-ae71-c59d11f48c9c>
2009-11-25 23:10:43 +0000

    Integrating webconsole with google visualization api.

--- betex-ecosystem/betex-ecosystem-webconsole/src/main/java/dk/betex/ecosystem/webconsole/client/Application.java
+++ betex-ecosystem/betex-ecosystem-webconsole/src/main/java/dk/betex/ecosystem/webconsole/client/Application.java
@@ -1,9 +1,20 @@
 package dk.betex.ecosystem.webconsole.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.user.client.Random;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
-
+import com.google.gwt.visualization.client.AbstractDataTable;
+import com.google.gwt.visualization.client.DataTable;
+import com.google.gwt.visualization.client.Selection;
+import com.google.gwt.visualization.client.VisualizationUtils;
+import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
+import com.google.gwt.visualization.client.events.SelectHandler;
+import com.google.gwt.visualization.client.visualizations.PieChart;
+import com.google.gwt.visualization.client.visualizations.PieChart.Options;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -17,7 +28,96 @@ public class Application
    */
   public void onModuleLoad()
   {
-     final Label label = new Label ( "gwt-maven-plugin Archetype :: Project dk.betex.ecosystem.betex-ecosystem-webconsole" );
-     RootPanel.get().add( label );
-  }
+	  // Create a callback to be called when the visualization API
+	    // has been loaded.
+	    Runnable onLoadCallback = new Runnable() {
+	      public void run() {
+	        Panel panel = RootPanel.get();
+	 
+	        // Create a pie chart visualization.
+	        final Options options = createOptions();
+	        final PieChart pie = new PieChart(createTable(), options);
+
+	       // pie.addSelectHandler(createSelectHandler(pie));
+	        panel.add(pie);
+	        
+	        Timer timer = new Timer(){
+			
+				@Override
+				public void run() {
+					pie.draw(createTable(),options);
+					
+				}
+			
+			};
+			timer.scheduleRepeating(100);
+	      }
+	    };
+
+	    // Load the visualization api, passing the onLoadCallback to be called
+	    // when loading is done.
+	    VisualizationUtils.loadVisualizationApi(onLoadCallback, PieChart.PACKAGE);
+	  }
+
+	  private Options createOptions() {
+	    Options options = Options.create();
+	    options.setWidth(400);
+	    options.setHeight(240);
+	    options.set3D(true);
+	    options.setTitle("My Daily Activities");
+	    return options;
+	  }
+
+	  private SelectHandler createSelectHandler(final PieChart chart) {
+	    return new SelectHandler() {
+	      @Override
+	      public void onSelect(SelectEvent event) {
+	        String message = "";
+	        
+	        // May be multiple selections.
+	        JsArray<Selection> selections = chart.getSelections();
+
+	        for (int i = 0; i < selections.length(); i++) {
+	          // add a new line for each selection
+	          message += i == 0 ? "" : "\n";
+	          
+	          Selection selection = selections.get(i);
+
+	          if (selection.isCell()) {
+	            // isCell() returns true if a cell has been selected.
+	            
+	            // getRow() returns the row number of the selected cell.
+	            int row = selection.getRow();
+	            // getColumn() returns the column number of the selected cell.
+	            int column = selection.getColumn();
+	            message += "cell " + row + ":" + column + " selected";
+	          } else if (selection.isRow()) {
+	            // isRow() returns true if an entire row has been selected.
+	            
+	            // getRow() returns the row number of the selected row.
+	            int row = selection.getRow();
+	            message += "row " + row + " selected";
+	          } else {
+	            // unreachable
+	            message += "Pie chart selections should be either row selections or cell selections.";
+	            message += "  Other visualizations support column selections as well.";
+	          }
+	        }
+	        
+	        Window.alert(message);
+	      }
+	    };
+	  }
+
+	  private AbstractDataTable createTable() {
+	    DataTable data = DataTable.create();
+	    data.addColumn(ColumnType.STRING, "Task");
+	    data.addColumn(ColumnType.NUMBER, "Hours per Day");
+	    data.addRows(2);
+	    data.setValue(0, 0, "Work");
+	    data.setValue(0, 1, Random.nextInt(100));
+	    data.setValue(1, 0, "Sleep");
+	    data.setValue(1, 1, Random.nextInt(100));
+	    return data;
+	  }
 }
