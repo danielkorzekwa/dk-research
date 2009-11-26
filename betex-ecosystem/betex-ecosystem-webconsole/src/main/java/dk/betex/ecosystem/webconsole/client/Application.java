e8b5d26163d4e475a8ba67ed3dbb2cb8ccd1be53
daniel.korzekwa <daniel.korzekwa@2d2c5f88-d9fa-11de-ae71-c59d11f48c9c>
2009-11-26 00:51:27 +0000

    Creating BioHeatMap visualization.

--- betex-ecosystem/betex-ecosystem-webconsole/src/main/java/dk/betex/ecosystem/webconsole/client/Application.java
+++ betex-ecosystem/betex-ecosystem-webconsole/src/main/java/dk/betex/ecosystem/webconsole/client/Application.java
@@ -1,123 +1,90 @@
 package dk.betex.ecosystem.webconsole.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.JsArray;
 import com.google.gwt.user.client.Random;
 import com.google.gwt.user.client.Timer;
-import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.visualization.client.AbstractDataTable;
 import com.google.gwt.visualization.client.DataTable;
-import com.google.gwt.visualization.client.Selection;
 import com.google.gwt.visualization.client.VisualizationUtils;
 import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
-import com.google.gwt.visualization.client.events.SelectHandler;
 import com.google.gwt.visualization.client.visualizations.PieChart;
-import com.google.gwt.visualization.client.visualizations.PieChart.Options;
+
+import dk.betex.ecosystem.webconsole.client.visualizations.BioHeatMap;
+import dk.betex.ecosystem.webconsole.client.visualizations.BioHeatMap.Options;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class Application
-    implements EntryPoint
-{
+public class Application implements EntryPoint {
 
-  /**
-   * This is the entry point method.
-   */
-  public void onModuleLoad()
-  {
-	  // Create a callback to be called when the visualization API
-	    // has been loaded.
-	    Runnable onLoadCallback = new Runnable() {
-	      public void run() {
-	        Panel panel = RootPanel.get();
-	 
-	        // Create a pie chart visualization.
-	        final Options options = createOptions();
-	        final PieChart pie = new PieChart(createTable(), options);
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		// Create a callback to be called when the visualization API
+		// has been loaded.
+		Runnable onLoadCallback = new Runnable() {
+			public void run() {
+				Panel panel = RootPanel.get();
 
-	       // pie.addSelectHandler(createSelectHandler(pie));
-	        panel.add(pie);
-	        
-	        Timer timer = new Timer(){
-			
-				@Override
-				public void run() {
-					pie.draw(createTable(),options);
-					
-				}
-			
-			};
-			timer.scheduleRepeating(100);
-	      }
-	    };
+				final Options options = BioHeatMap.Options.create();
+				options.setHeight(50);
+				options.setWidth(50);
+				options.setWidth(300);
+				options.setHeight(500);
+				final BioHeatMap bioHeatMap = new BioHeatMap(createTable(), options);
+				panel.add(bioHeatMap);
 
-	    // Load the visualization api, passing the onLoadCallback to be called
-	    // when loading is done.
-	    VisualizationUtils.loadVisualizationApi(onLoadCallback, PieChart.PACKAGE);
-	  }
+				final Timer timer = new Timer() {
 
-	  private Options createOptions() {
-	    Options options = Options.create();
-	    options.setWidth(400);
-	    options.setHeight(240);
-	    options.set3D(true);
-	    options.setTitle("My Daily Activities");
-	    return options;
-	  }
+					@Override
+					public void run() {
+						bioHeatMap.draw(createTable(), options);
 
-	  private SelectHandler createSelectHandler(final PieChart chart) {
-	    return new SelectHandler() {
-	      @Override
-	      public void onSelect(SelectEvent event) {
-	        String message = "";
-	        
-	        // May be multiple selections.
-	        JsArray<Selection> selections = chart.getSelections();
+					}
 
-	        for (int i = 0; i < selections.length(); i++) {
-	          // add a new line for each selection
-	          message += i == 0 ? "" : "\n";
-	          
-	          Selection selection = selections.get(i);
+				};
+				
+				Button start = new Button("start");
+				panel.add(start);
+				start.addClickListener(new ClickListener() {
+				
+					@Override
+					public void onClick(Widget arg0) {
+						timer.scheduleRepeating(1000/10);
+					}
+				
+				});
+				
+			}
+		};
 
-	          if (selection.isCell()) {
-	            // isCell() returns true if a cell has been selected.
-	            
-	            // getRow() returns the row number of the selected cell.
-	            int row = selection.getRow();
-	            // getColumn() returns the column number of the selected cell.
-	            int column = selection.getColumn();
-	            message += "cell " + row + ":" + column + " selected";
-	          } else if (selection.isRow()) {
-	            // isRow() returns true if an entire row has been selected.
-	            
-	            // getRow() returns the row number of the selected row.
-	            int row = selection.getRow();
-	            message += "row " + row + " selected";
-	          } else {
-	            // unreachable
-	            message += "Pie chart selections should be either row selections or cell selections.";
-	            message += "  Other visualizations support column selections as well.";
-	          }
-	        }
-	        
-	        Window.alert(message);
-	      }
-	    };
-	  }
+		// Load the visualization api, passing the onLoadCallback to be called
+		// when loading is done.
+		VisualizationUtils.loadVisualizationApi(onLoadCallback, PieChart.PACKAGE);
 
-	  private AbstractDataTable createTable() {
-	    DataTable data = DataTable.create();
-	    data.addColumn(ColumnType.STRING, "Task");
-	    data.addColumn(ColumnType.NUMBER, "Hours per Day");
-	    data.addRows(2);
-	    data.setValue(0, 0, "Work");
-	    data.setValue(0, 1, Random.nextInt(100));
-	    data.setValue(1, 0, "Sleep");
-	    data.setValue(1, 1, Random.nextInt(100));
-	    return data;
-	  }
+	}
+
+	private AbstractDataTable createTable() {
+		DataTable data = DataTable.create();
+		data.addColumn(ColumnType.STRING, "Price");
+		for (int runnerId = 0; runnerId < 10; runnerId++) {
+			data.addColumn(ColumnType.NUMBER, "Runner " + runnerId);
+		}
+
+		data.addRows(100);
+		for (int price = 0; price < 100; price++) {
+			data.setValue(price, 0, "" + price);
+			for (int runnerId = 0; runnerId < 10; runnerId++) {
+				data.setValue(price, runnerId + 1, Random.nextInt(100));
+			}
+		}
+
+		return data;
+	}
 }
